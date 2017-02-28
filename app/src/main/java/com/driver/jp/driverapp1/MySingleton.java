50dccbbd5b095741f697d5ff703ab680f9a1fb50
Jp <jsprouse@ggc.edu>
2017-02-27 21:38:39 -0500

    setup

---
+++ app/src/main/java/com/driver/jp/driverapp1/MySingleton.java
@@ -0,0 +1,41 @@
+package com.driver.jp.driverapp1;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Jp on 027, 2/27/17.
+ */
+
+public class MySingleton {
+    private static MySingleton mInstance;
+    private RequestQueue requestQueue;
+    private static Context mCtx;
+
+    private MySingleton(Context context){
+        mCtx = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized MySingleton getInstance(Context context){
+        if(mInstance==null){
+            mInstance= new MySingleton(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        if(getRequestQueue()==null){
+            requestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T>void addTorequestque(Request<T> request){
+        requestQueue.add(request);
+    }
+
+}
