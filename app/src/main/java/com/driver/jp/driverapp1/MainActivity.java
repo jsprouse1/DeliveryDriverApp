a4269111474f390da0eccdd5d3e2858928ab1f32
Jp <jsprouse@ggc.edu>
2017-02-27 21:06:04 -0500

    Init

---
+++ app/src/main/java/com/driver/jp/driverapp1/MainActivity.java
@@ -0,0 +1,81 @@
+package com.driver.jp.driverapp1;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText Name, Password;
+    String server_url = "http://192.168.50.4/DriverApp/new_user.php";
+    AlertDialog.Builder builder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.buttonSubmit);
+        Name = (EditText)findViewById(R.id.fName);
+        Password = (EditText)findViewById(R.id.password);
+        builder = new AlertDialog.Builder(MainActivity.this);
+        button.setOnClickListener(new View.OnClickListener(){
+           @Override
+            public void onClick(View v){
+               final String name, password;
+                name = Name.getText().toString();
+                password = Password.getText().toString();
+               StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
+                       new Response.Listener<String>(){
+                           @Override
+                           public void onResponse(String response){
+                            builder.setTitle("Server Response");
+                               builder.setMessage("Response :"+response);
+                               builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                                   @Override
+                                   public void onClick(DialogInterface dialog, int which){
+                                       Name.setText("");
+                                       Password.setText("");
+                                   }
+                               });
+                               AlertDialog alertDialog = builder.create();
+                               alertDialog.show();
+                           }
+                       }
+               , new Response.ErrorListener() {
+                   @Override
+                   public void onErrorResponse(VolleyError error){
+                       Toast.makeText(MainActivity.this, "I'm afraid I can't do that Dave.", Toast.LENGTH_SHORT).show();
+                       error.printStackTrace();
+                   }
+               }){
+                   @Override
+                   protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("fName", name);
+                        params.put("password", password);
+                        return params;
+                   }
+               };
+
+               MySingleton.getInstance(MainActivity.this).addTorequestque(stringRequest);
+
+
+           }
+        });
+    }
+}
